dc022837dbe9e70ab6167421c023338100ff1e35
NoFoster <kfoster0@chicagobooth.edu>
2025-04-24 22:57:15 +0000

    Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 
+
+  get ("/dice/:number_of_dice/:sides", { :controller => "dice", :action => "rolls"})
+
+  get("/", { :controller => "dice", :action => "homepage"})
 end
